e243809d998136cfdb5126230bab5995aa2574c2
Gropp <fgropp@gmail.com>
2022-08-18 20:18:19 -0300

    levantando o servidor

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello World!')
+//o express é um gerenciador de rotas http
+import express, { Request, Response, NextFunction} from "express";
+
+//instancia a biblioteca
+const app = express();
+
+//definindo a PORTA do servidor
+const PORT = 3000
+
+//quando chegar uma requisicao (GET) em /status o aplicativo roda o codigo da funcao arrow
+//o express sempre trabalha com request, results e next
+//ele vai retornar um JSON (foo)
+//com o TS podemos tipar os argumentos
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+//precisamos que o servidor fique monitarando as requisicoes em uma porta
+app.listen(PORT, () => {
+    console.log(`A aplicação esta sendo executada na porta: ${PORT}`);
+});
