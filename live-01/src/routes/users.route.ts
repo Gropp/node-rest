6041355f99c5fe0e26f419ae959f6ae960605b3b
Gropp <fgropp@gmail.com>
2022-08-18 21:16:23 -0300

    StatusCodes

--- live-01/src/routes/users.route.ts
+++ live-01/src/routes/users.route.ts
@@ -3,14 +3,28 @@
 //USANDO O EXPRESS PARA FAZER AS ROTAS
 
 import { NextFunction, Request, Response, Router } from "express";
+//TUDO QUE ENTRA NA REQUISICAO VEM PELO REQUESTE - REQ
+//TUDO QUE SAI DA REQUISICAO SAO PELO RESPONSE - RES
+
+import { StatusCodes } from "http-status-codes";
+//BIBLIOTECA COM AS CONSTANTES DOS CODIGOS HTTP
 
 //criando configuracoes de rotas sem estar amarrado na instancia de aplicacao
 //instancia a funcao
 const usersRoute = Router();
 
+//ROTA PARA GET /USERS - LISTAR TODOS
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Fernando'}];
-    res.status(200).send({users});
+    res.status(StatusCodes.OK).send(users);
+});
+
+//ROTA PARA GET /USERS/:UUID - LISTA CLIENTE ESPECIFICO
+//CONSEGUIMOS TIPIFICAR O TIPO DE DADO DO UUID
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    //aqui voce coloca o banco de dados e faz um select nos users usando o uuid como where
+    res.status(StatusCodes.OK).send({uuid});
 });
 
 export default usersRoute;
